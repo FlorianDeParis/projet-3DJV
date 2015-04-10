dd005b129556b00696366c369fc797c0c116ac36
FlorianDeParis <florian.tournay@hotmail.fr>
2015-04-10 12:17:04 +0200

    test gitignore

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_NAME', 'projet-3djv');
 define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'test');
+define('DB_PASSWORD', '');
 
 /** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
