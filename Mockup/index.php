0c5bc003de9a32a86f995a255b65eeb3b352beb5
FlorianDeParis <florian.tournay@hotmail.fr>
2015-05-03 21:05:03 +0200

    ..

---
+++ Mockup/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Mockup du site de l'association 3D & Jeux Vidéo</title>
+		<meta name="description" content="ESGI 3DJV - 3D et Jeux Vidéos">
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="./js/script.js" type="text/javascript"></script>
+		<link rel="stylesheet" type="text/css" media="screen" href="./css/style.css" />
+	</head>
+	<body>
+		<nav>
+			<img scr="./img/logo.png" alt="Logo ESGI 3DJV">
+			<ul>
+				<li>TP</li>
+				<li>TUTOS</li>
+				<li>ACTUS</li>
+				<li>MEDIAS</li>
+			</ul>
+			<form><input type="text" name="rechercher" value="Rechercher"></form>
+		</nav>
+	</body>
+</html>
